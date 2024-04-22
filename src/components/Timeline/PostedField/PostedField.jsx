4dbab875943c1b98fdc678e30d245390d6ba26b6
practice_user <practice@mail.co.jp>
2024-04-23 04:52:57 +0900

    posted field

--- src/components/Timeline/PostedField/PostedField.jsx
+++ src/components/Timeline/PostedField/PostedField.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PostedField.css";
+import { Analytics, Face, Gif, Photo } from "@mui/icons-material";
 export const PostedField = () => {
   return (
     <div className="postedField">
@@ -17,7 +18,27 @@ export const PostedField = () => {
           />
         </div>
         <hr className="postedFieldHr" />
-        <div className="iconArea">piyo</div>
+        <div className="iconArea">
+          <div className="postOptions">
+            <div className="postOption">
+              <Photo htmlColor="blue" />
+              <p>写真</p>
+            </div>
+            <div className="postOption">
+              <Gif htmlColor="red" />
+              <p>GIF</p>
+            </div>
+            <div className="postOption">
+              <Face htmlColor="green" />
+              <p>気持ち</p>
+            </div>
+            <div className="postOption">
+              <Analytics htmlColor="red" />
+              <p>投票</p>
+            </div>
+          </div>
+          <button className="postButton">投稿</button>
+        </div>
       </div>
     </div>
   );
